4ac5c13cf976a9b8fbb9572206c3c75d02fdd35d
moritz <moritz.loe@gmail.com>
2015-05-25 18:04:13 +0200

    init

---
+++ src/main/java/de/matze/App.java
@@ -0,0 +1,13 @@
+package de.matze;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
